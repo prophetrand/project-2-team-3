18f543fe191e9842aa286497f5d38c5bd62e360e
Rand Hale <randluca@gmail.com>
2021-02-06 14:45:40 -0800

    require additional NPMs and add validation to User model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+var bcrypt = require("bcryptjs");
+
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
         username: {
@@ -29,6 +31,14 @@ module.exports = function(sequelize, DataTypes) {
         }
     },{
         freezeTableName: true
+    });
+
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+      };
+      // Password hashing functionality
+    User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
     });
 
     return User;
