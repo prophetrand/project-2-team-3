2bcb88c924c04ca88c68b36baaae9e5a50a0b7ec
Rand Hale <randluca@gmail.com>
2021-02-06 14:37:52 -0800

    write skeleton routes for api and html

---
+++ routes/api-routes.js
@@ -0,0 +1,53 @@
+var db = require("../models");
+var passport = require("../config/passport");
+
+module.exports = function(app) {
+
+    app.get("/api/signup", function(req, res) {
+        db.User.create({
+          username: req.body.username,
+          password: req.body.password
+        })
+          .then(function() {
+            res.redirect(307, "/login");
+          })
+          .catch(function(err) {
+            res.status(401).json(err);
+          });
+    });
+
+    app.get("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json(req.user);
+    });
+
+    app.get("/api/signup", passport.authenticate("local"), function(req, res) {
+        
+    });
+
+    app.get("/api/user", function(req, res) {
+
+    });
+
+    app.get("/api/user/:id", function(req, res) {
+
+    });
+
+    app.post("/api/user", function(req, res) {
+
+    });
+
+    app.get("/api/connect", function(req, res) {
+
+    });
+
+    app.get("/api/matches/:id", function(req, res) {
+
+    });
+
+    app.post("/api/user/:id", function(req, res) {
+
+        // Update user bio, interests, picture
+        // Updates the matches model
+
+    });
+}
