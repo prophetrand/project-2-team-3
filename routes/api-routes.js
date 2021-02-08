a2e7afd21a70e01d7530d1ab4278f8f195b328d6
jessny <jessnyjoseph@gmail.com>
2021-02-07 20:23:02 -0800

    added api/user/:id route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -32,7 +32,14 @@ module.exports = function(app) {
     });
 
     app.get("/api/user/:id", function(req, res) {
-
+      db.User.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbUser) {
+        res.json(dbUser);
+      });
     });
 
     app.post("/api/user", function(req, res) {
