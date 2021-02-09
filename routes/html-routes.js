3560f92c11a1229b0174895c4a73198b2cb6ffbc
Rand Hale <randluca@gmail.com>
2021-02-09 11:22:38 -0800

    adding code for /profile html-route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     });
 
     app.get("/profile", function(req, res) {
-        
+        res.render("profile");
     });
 
     app.get("/signup", function(req, res) {
