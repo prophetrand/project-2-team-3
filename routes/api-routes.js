585a49f2e2cbf0438178c8f534a64d605e7f7907
Rand Hale <randluca@gmail.com>
2021-02-09 13:09:42 -0800

    slight changes to api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,42 +6,52 @@ module.exports = function(app) {
     app.post("/api/signup", function(req, res) {
         db.User.create({
           username: req.body.username,
-          password: req.body.password,
-          bio: req.body.bio
+          password: req.body.password
         })
-          .then(function(data) {
-            res.redirect(307, "/api/login");
+          .then(function() {
+            res.redirect(307, "/login");
           })
           .catch(function(err) {
-            console.log(err);
             res.status(401).json(err);
           });
     });
 
-    app.post("/api/interests", function(req, res) {
-      db.User_interests.create({
-        user_id: req.user.id,
-        interest_id: req.body.interest_id
-      })
-        .then(function(data) {
-          res.send(data);
-        })
-        .catch(function(err) {
-          console.log(err);
-          res.status(401).json(err);
-        });
-    });
-
-    app.post("/api/login", passport.authenticate("local"), function(req, res) { 
-      res.json(req.user);
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json(req.user);
     });
 
     app.get("/api/user", function(req, res) {
-
+      db.User.find({})
+      .then(function(dbUser) {
+        res.json(dbUser);
+      });
     });
 
     app.get("/api/user/:id", function(req, res) {
+      db.User.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbUser) {
+        res.json(dbUser);
+      });
+    });
 
+    // Put route for updating users
+    app.put("/api/user", function(req, res) {
+      db.User.update({
+        profPic: req.body.profPic,
+        interests: req.body.interests,
+        bio: req.body.bio 
+      }, {
+          where: {
+          id: req.body.id
+        }
+      })
+      .then(function(dbUser) {
+        res.json(dbUser);
+      });
     });
 
     app.get("/api/connect", function(req, res) {
@@ -52,9 +62,4 @@ module.exports = function(app) {
 
     });
 
-    // GET route for logout function, will probably be useful later.
-    // app.get("/logout", function(req, res) {
-    //   req.logout();
-    //   res.redirect("/");
-    // });
 }
