5ec1e87bd6a084f47b8f53864f6ba3bc4aa5e37c
Rand Hale <randluca@gmail.com>
2021-02-09 15:20:58 -0800

    updating models with proper many-to-many associations

--- models/interests.js
+++ models/interests.js
@@ -1,3 +1,6 @@
+const user_interests = require("./user_interests");
+const User = require("./user");
+
 module.exports = function(sequelize, DataTypes) {
     var Interests = sequelize.define("Interests", {
         name: {
@@ -7,16 +10,16 @@ module.exports = function(sequelize, DataTypes) {
               len: [1]
             }
         },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              len: [1]
-            }
-        },
     },{
         freezeTableName: true
     });
-
+    Interests.associate = function(models) {
+        Interests.belongsToMany(models.User, {
+         through: models.User_interests,
+         as: "User",
+         foreignKey: "interest_id"
+        });
+    }
+    
     return Interests;
 };
