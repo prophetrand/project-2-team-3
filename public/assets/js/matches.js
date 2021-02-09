4229e874e1cd6e60937978a1ab08fda162321558
Abuye Mamuye <abuyeye3@gmail.com>
2021-02-09 12:27:51 -0800

    add code on the matches.s

--- public/assets/js/matches.js
+++ public/assets/js/matches.js
@@ -3,13 +3,12 @@
 
 $(document).ready(function() {
     
-    // $.get("/api/veiws/layouts/matches.handlebars").then(function(data) {
-     
-    // });
-    
-    $.get("/api/user_data").then(function(data) {
-      $(".matches-name").text(data.email);
+    $.get("/api/veiws/layouts/matches.handlebars").then(function(data) {
+      $(".matches-name").text(data);
     });
+    
+    
+    
     
   });
 
